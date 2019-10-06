6d710bf705af883cd3f41892e15ab80338f73927
李想 <48878834+asdhlasdklsa@users.noreply.github.com>
2019-10-06 09:07:30 -0500

    Add files via upload

---
+++ 判断质数.java
@@ -0,0 +1,23 @@
+package 学习成绩;
+
+public class 判断质数 {
+	public static void main(String[] args) {
+		int count = 0;
+		for ( int i =2 ; i <= 10000 ; i ++ ) {
+			boolean  素数 = true ;
+			for ( int j = 2 ; j < i ; j ++) {
+				if (i % j == 0 ) {
+					素数 = false;
+					break;
+				}
+			}
+			if (素数) {
+				System.out.print(i+"  ");
+				count++;
+				if (count % 8 == 0) {
+					System.out.println();
+				}
+			}		
+		}
+	}
+}
